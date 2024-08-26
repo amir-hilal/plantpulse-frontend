20a201e340d50a4de782f48036f1e796cc4dd375
amirhilal044 <amiramirhilal@gmail.com>
2024-08-26 10:30:19 +0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/common/NavBar';
+import Footer from './components/common/Footer';
+import LandingPage from './components/pages/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const userProfile = {
+        picture: "/path-to-user-profile-picture.png"
+    };
+
+    return (
+        <Router>
+            <div className="App">
+                <NavBar isLoggedIn={isLoggedIn} userProfile={userProfile} />
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        {/* Add more routes here, e.g., HomePage, MyGardenPage, etc. */}
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
