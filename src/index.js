ba1872566ca8279f4087cc44b4b47a1503867a63
amirhilal044 <amiramirhilal@gmail.com>
2024-08-26 15:02:10 +0300

    added slick carousel package

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import store from './store/store'; // Adjust the path according to your folder structure
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
