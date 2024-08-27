393734cd11ccd84e9a011caef188bf6b4161b488
amirhilal044 <amiramirhilal@gmail.com>
2024-08-27 20:24:58 +0300

    changed authslice logic

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,24 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    isLoggedIn: false,
-    userProfile: null,
+  isLoggedIn: !!localStorage.getItem('token'), 
+  userProfile: null,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        login(state, action) {
-            state.isLoggedIn = true;
-            state.userProfile = action.payload;
-        },
-        logout(state) {
-            state.isLoggedIn = false;
-            state.userProfile = null;
-        },
+  name: 'auth',
+  initialState,
+  reducers: {
+    login(state, action) {
+      state.isLoggedIn = true;
+      state.userProfile = action.payload.user;
+      localStorage.setItem('token', action.payload.token);
     },
+    logout(state) {
+      state.isLoggedIn = false;
+      state.userProfile = null;
+      localStorage.removeItem('token');
+    },
+    setUserProfile(state, action) {
+      state.userProfile = action.payload;
+    },
+  },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, setUserProfile } = authSlice.actions;
 export default authSlice.reducer;
