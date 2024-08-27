0b3e1e774712c7c2d6dac17973e4ada3a66c23b6
amirhilal044 <amiramirhilal@gmail.com>
2024-08-27 15:40:50 +0300

    styled login

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Loading from 'react-loading';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import logo from '../assets/images/Logo_black.png';
@@ -44,52 +44,66 @@ const LoginPage = () => {
   };
 
   return (
-    <div className=" flex flex-column align-items-center bg-tint-5 border-round w-8">
-      <ToastContainer />
-      <div className="flex flex-column align-items-center w-10">
-        <img
-          src={logo}
-          alt="PlantPulse"
-          className="logo"
-          onClick={() => navigate('/')}
-        />
-        <h2 className="text-center">Log in to PlantPulse</h2>
+    <div className="w-screen h-screen flex flex-column justify-content-center align-items-center">
+      <img
+        src={logo}
+        alt="PlantPulse"
+        className="logo mb-6"
+        onClick={() => navigate('/')}
+      />
+      <div className=" flex flex-column align-items-center bg-tint-5 border-round-xl w-6 p-5">
+        <ToastContainer />
+        <div className="flex flex-column align-items-center w-10">
+          <h2 className="text-center">Log in to PlantPulse</h2>
+        </div>
+        <form className="formgrid grid w-12 md:w-10" onSubmit={handleSubmit}>
+          <div className="field col-12 flex p-0">
+            <input
+              type="text"
+              name="emailOrUsername"
+              placeholder="Email or Username"
+              value={formData.emailOrUsername}
+              onChange={handleChange}
+              required
+              class="text-xs md:text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+            />
+          </div>
+          <div class="field col-12 flex p-0">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              class="text-xs md:text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+              required
+            />
+          </div>
+          <div class="field col-12 flex p-0">
+            <Link
+              className="font-10 text-grey cursor-pointer"
+              to="/forgot-password"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+          <button type="submit" className="p-button w-full">
+            {isLoading ? (
+              <Loading type="spin" color="#fff" height={20} width={20} />
+            ) : (
+              'Login'
+            )}
+          </button>
+          <div class="field col-12 flex p-0">
+
+          <p className='font-10 text-grey cursor-pointer'>
+            Don’t have an account?{' '}
+            <Link to="/register">Sign up for PlantPulse</Link>
+            </p>
+          </div>
+
+        </form>
       </div>
-      <form className="formgrid grid w-10" onSubmit={handleSubmit}>
-        <div className="field col-12 flex p-0">
-          <input
-            type="text"
-            name="emailOrUsername"
-            placeholder="Email or Username"
-            value={formData.emailOrUsername}
-            onChange={handleChange}
-            required
-            class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
-          />
-        </div>
-        <div class="field col-12 flex p-0">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
-            required
-          />
-        </div>
-        <a href="/forgot-password">Forgot Password?</a>
-        <button type="submit" className="p-button w-full">
-          {isLoading ? (
-            <Loading type="spin" color="#fff" height={20} width={20} />
-          ) : (
-            'Login'
-          )}
-        </button>
-        <p>
-          Don’t have an account? <a href="/register">Sign up for PlantPulse</a>
-        </p>
-      </form>
     </div>
   );
 };
