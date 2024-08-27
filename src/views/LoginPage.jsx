65206025e66fdc81cd865a321b0e1a3febcf11c9
amirhilal044 <amiramirhilal@gmail.com>
2024-08-27 15:06:48 +0300

    styling login page

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import logo from '../assets/images/Logo_black.png';
 import { login } from '../features/auth/authSlice';
 import api from '../services/api';
 
@@ -43,34 +44,42 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="login-page">
+    <div className=" flex flex-column align-items-center bg-tint-5 border-round w-8">
       <ToastContainer />
-      <form className="login-form" onSubmit={handleSubmit}>
+      <div className="flex flex-column align-items-center w-10">
         <img
-          src="/path-to-your-logo.png"
+          src={logo}
           alt="PlantPulse"
           className="logo"
           onClick={() => navigate('/')}
         />
-        <h2>Log in to PlantPulse</h2>
-        <input
-          type="text"
-          name="emailOrUsername"
-          placeholder="Email or Username"
-          value={formData.emailOrUsername}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
+        <h2 className="text-center">Log in to PlantPulse</h2>
+      </div>
+      <form className="formgrid grid w-10" onSubmit={handleSubmit}>
+        <div className="field col-12 flex p-0">
+          <input
+            type="text"
+            name="emailOrUsername"
+            placeholder="Email or Username"
+            value={formData.emailOrUsername}
+            onChange={handleChange}
+            required
+            class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+          />
+        </div>
+        <div class="field col-12 flex p-0">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+            required
+          />
+        </div>
         <a href="/forgot-password">Forgot Password?</a>
-        <button type="submit" className="p-button p-component p-button-success">
+        <button type="submit" className="p-button w-full">
           {isLoading ? (
             <Loading type="spin" color="#fff" height={20} width={20} />
           ) : (
