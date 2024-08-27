a91cd19fcd9571a771233f58db84578227de6215
amirhilal044 <amiramirhilal@gmail.com>
2024-08-27 20:37:38 +0300

    added fetchUserProfile function <> storing token in the localsorage and return user data to store in redux

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,16 +1,34 @@
-import api from './api';
+import api from './api'; 
 
 export const loginUser = async (credentials) => {
-  const response = await api.post('/login', credentials);
-  return response.data;
+  try {
+    const response = await api.post('/login', credentials);
+    const { token, user } = response.data;
+    localStorage.setItem('token', token);
+
+    return user;
+  } catch (error) {
+    throw error.response.data.error;
+  }
 };
 
 export const registerUser = async (userData) => {
-  const response = await api.post('/register', userData);
-  return response.data;
+  try {
+    const response = await api.post('/register', userData);
+    const { token, user } = response.data;
+    localStorage.setItem('token', token);
+
+    return user;
+  } catch (error) {
+    throw error.response.data.error;
+  }
 };
 
-export const fetchUserProfile = async () => {
-  const response = await api.get('/profile');
-  return response.data;
+export const fetchUserData = async () => {
+  try {
+    const response = await api.get('/me');
+    return response.data.user;
+  } catch (error) {
+    throw error.response.data.error;
+  }
 };
