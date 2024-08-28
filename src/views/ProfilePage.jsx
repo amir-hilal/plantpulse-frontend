bff2bba80662b26b4e12f11717f20c74713d7c71
amirhilal044 <amiramirhilal@gmail.com>
2024-08-28 21:06:03 +0300

    passing props from profilepage to profileheader

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ProfileHeader from '../components/ProfileHeader';
 
 const ProfilePage = () => {
+    const user = useSelector((state) => state.auth.userProfile);
+
     return (
         <div className="profile-page">
-            <ProfileHeader />
+            <ProfileHeader
+                profilePicture={user.profile_photo_url}
+                coverPhoto={user.cover_photo_url}
+                firstName={user.first_name}
+                lastName={user.last_name}
+                username={user.username}
+            />
             {/* Additional profile page content can go here */}
         </div>
     );
