fed60c728c93d37ee162e7ce68d4d01756242710
amirhilal044 <amiramirhilal@gmail.com>
2024-08-30 17:02:21 +0300

    filled form with prev data

--- src/components/Profile/EditProfileModal.jsx
+++ src/components/Profile/EditProfileModal.jsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { IoIosArrowDown } from 'react-icons/io';
+
 const EditProfileModal = ({ isOpen, onClose, userProfile }) => {
   const [formData, setFormData] = useState({
-    first_name: userProfile.first_name,
-    last_name: userProfile.last_name,
-    phone_number: userProfile.phone_number || '',
-    address: userProfile.address || '',
-    birthday: userProfile.birthday || '',
-    gender: userProfile.gender || '',
-    about: userProfile.about || '',
-    profile_photo_url: userProfile.profile_photo_url || '',
+    first_name: '',
+    last_name: '',
+    phone_number: '',
+    address: '',
+    birthday: '',
+    gender: '',
+    about: '',
+    profile_photo_url: '',
   });
 
   useEffect(() => {
     if (isOpen) {
       document.body.classList.add('overflow-hidden');
+      setFormData({
+        first_name: userProfile.first_name || '',
+        last_name: userProfile.last_name || '',
+        phone_number: userProfile.phone_number || '',
+        address: userProfile.address || '',
+        birthday: userProfile.birthday || '',
+        gender: userProfile.gender || '',
+        about: userProfile.about || '',
+        profile_photo_url: userProfile.profile_photo_url || '',
+      });
     } else {
       document.body.classList.remove('overflow-hidden');
     }
@@ -23,7 +34,7 @@ const EditProfileModal = ({ isOpen, onClose, userProfile }) => {
     return () => {
       document.body.classList.remove('overflow-hidden');
     };
-  }, [isOpen]);
+  }, [isOpen, userProfile]);
 
   const handleChange = (e) => {
     setFormData({
@@ -34,16 +45,18 @@ const EditProfileModal = ({ isOpen, onClose, userProfile }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Handle form submission logic here
+    console.log('Form submitted:', formData);
   };
 
   if (!isOpen) return null;
 
   return (
     <div
-      className="fixed top-0 left-0 w-full h-full flex justify-content-center align-items-center "
+      className="fixed top-0 left-0 w-full h-full flex justify-content-center align-items-center"
       style={{ backgroundColor: 'rgba(22, 29, 33, 0.5)' }}
     >
-      <div className="relative bg-white border-round-xl p-6 w-11 sm:w-8 md:w-8 lg:w-8 shadow-4 overflow-x-auto md:overflow-hidden max-h-30rem h-30rem">
+      <div className="relative bg-white border-round-xl p-6 w-11 sm:w-8 md:w-8 lg:w-8 shadow-4 overflow-x-auto md:overflow-hidden h-30rem flex justify-content-center align-items-center">
         <button
           onClick={onClose}
           className="text-secondary bg-transparent absolute top-0 right-0 mt-3 mr-3 cursor-pointer border-none p-0"
@@ -51,89 +64,79 @@ const EditProfileModal = ({ isOpen, onClose, userProfile }) => {
           <FaTimes className="text-2xl text-secondary" />
         </button>
 
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-column sm:flex-row align-items-center">
+        <form onSubmit={handleSubmit} className="flex flex-column mt-8 md:mt-0">
+          <div className="flex flex-column sm:flex-row align-items-center mt-8 md:mt-0">
             <img
               src={formData.profile_photo_url}
               alt="Profile"
-              className="h-8rem w-8rem border-circle shadow-2 mr-0 mb-3 sm:mr-5 sm:mb-0 "
+              className="h-8rem w-8rem border-circle shadow-2 mr-0 mb-3 mt-8 sm:mt-0 sm:mr-5 sm:mb-0"
             />
 
-            <div className="formgrid grid ml-0">
+            <div className="formgrid grid ml-0 mr-0">
               <div className="field col-12 md:col-6 flex flex-column p-0 md:pr-1">
-                <label htmlFor="firstname">Firstname</label>
-
+                <label htmlFor="first_name">Firstname</label>
                 <input
                   type="text"
-                  name="firstname"
+                  name="first_name"
                   placeholder="First Name"
-                  value={formData.emailOrUsername}
+                  value={formData.first_name}
                   onChange={handleChange}
                   required
-                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-3 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
+                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
                 />
               </div>
               <div className="field col-12 md:col-6 flex flex-column p-0 md:pl-1">
-                <label htmlFor="lastname">Lastname</label>
-
+                <label htmlFor="last_name">Lastname</label>
                 <input
                   type="text"
-                  name="lastname"
+                  name="last_name"
                   placeholder="Last Name"
-                  value={formData.emailOrUsername}
+                  value={formData.last_name}
                   onChange={handleChange}
                   required
-                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-3 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
+                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
                 />
               </div>
               <div className="field col-12 md:col-6 flex flex-column p-0 md:pr-1">
-                <label htmlFor="phoneNumber">Phone</label>
-
+                <label htmlFor="phone_number">Phone</label>
                 <input
-                  type="text"
-                  name="phoneNumber"
+                  type="tel"
+                  name="phone_number"
                   placeholder="Phone"
-                  value={formData.phoneNumber}
+                  value={formData.phone_number}
                   onChange={handleChange}
-                  required
-                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-3 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
+                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
                 />
               </div>
               <div className="field col-12 md:col-6 flex flex-column p-0 md:pl-1">
                 <label htmlFor="address">Address</label>
-
                 <input
                   type="text"
                   name="address"
                   placeholder="Address"
                   value={formData.address}
                   onChange={handleChange}
-                  required
-                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-3 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
+                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
                 />
               </div>
               <div className="field col-12 md:col-6 flex flex-column p-0 md:pr-1">
                 <label htmlFor="birthday">Birthday</label>
-
                 <input
                   type="date"
                   name="birthday"
                   placeholder="Birthday"
                   value={formData.birthday}
                   onChange={handleChange}
-                  required
-                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-3 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
+                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
                 />
               </div>
               <div className="relative field col-12 md:col-6 flex flex-column p-0 md:pl-1">
                 <label htmlFor="gender">Gender</label>
-
                 <select
                   name="gender"
                   value={formData.gender}
                   onChange={handleChange}
-                  required
-                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-3 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
+                  className="bg-tint-5 text-xs md:text-base text-color surface-overlay p-2 h-full border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
                 >
                   <option value="" disabled>
                     Gender
@@ -148,22 +151,23 @@ const EditProfileModal = ({ isOpen, onClose, userProfile }) => {
                 <label htmlFor="about">About</label>
                 <textarea
                   id="about"
+                  name="about"
                   type="text"
                   rows="4"
-                  className="bg-tint-5 text-base text-color surface-overlay p-3 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
+                  value={formData.about}
+                  onChange={handleChange}
+                  className="bg-tint-5 text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-auto"
                 ></textarea>
               </div>
             </div>
           </div>
-          <div>
           <div className="flex justify-content-end mt-4">
-              <button
-                type="submit"
-                className="w-10rem border-none  bg-primary text-white  border-round py-3 cursor-pointer"
-              >
-                Save
-              </button>
-            </div>
+            <button
+              type="submit"
+              className="w-10rem border-none bg-primary text-white border-round py-2 cursor-pointer"
+            >
+              Save
+            </button>
           </div>
         </form>
       </div>
