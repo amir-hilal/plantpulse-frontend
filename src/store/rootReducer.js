c69f56dea58bd23f6b9721079724fe1065dcfb66
amirhilal044 <amiramirhilal@gmail.com>
2024-09-02 15:35:09 +0300

    registered friends slice in store

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux';
 import authReducer from '../features/auth/authSlice';
 import uiReducer from '../features/ui/uiSlice';
 import postsReducer from '../features/community/postsSlice'; // Import the posts slice
-
+import friendsReducer from '../features/community/friendsSlice'
 const rootReducer = combineReducers({
     auth: authReducer,
     posts: postsReducer,
+    friends: friendsReducer,
     ui: uiReducer,
 });
 
