c737b6efa00362130642d131546499a9184b625c
amirhilal044 <amiramirhilal@gmail.com>
2024-09-02 14:03:48 +0300

    registered postsSlice

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from '../features/auth/authSlice';
-import uiReducer from '../features/ui/uiSlice'; 
+import uiReducer from '../features/ui/uiSlice';
+import postsReducer from '../features/community/postsSlice'; // Import the posts slice
 
 const rootReducer = combineReducers({
     auth: authReducer,
+    posts: postsReducer,
     ui: uiReducer,
 });
 
