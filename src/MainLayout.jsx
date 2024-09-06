437f2c7fbd77b32d778ef93c7f9670cb8ad3f84f
amirhilal044 <amiramirhilal@gmail.com>
2024-09-06 18:01:50 +0300

    added condition to check if the current route is home or landing page

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import VerifyEmailNotice from './components/common/VerifyEmailNotice';
 import Footer from './components/layouts/Footer';
 import NavBar from './components/layouts/NavBar';
 import { closeMenu } from './features/ui/uiSlice';
+import routes from './routes'; // Assuming you have defined routes in a routes.js file
 
 const MainLayout = ({ children }) => {
   const isMenuOpen = useSelector((state) => state.ui.isMenuOpen);
   const dispatch = useDispatch();
+  const location = useLocation(); // Get the current route
+
   const handleOverlayClick = () => {
     if (isMenuOpen) {
       dispatch(closeMenu());
     }
   };
+
+  // Condition to check if the current route is home or landing page
+  const showFooter =
+    location.pathname === routes.home || location.pathname === routes.landing;
+
   return (
     <>
       <ToastContainer />
@@ -25,7 +34,8 @@ const MainLayout = ({ children }) => {
       >
         <VerifyEmailNotice />
         <main>{children}</main>
-        <Footer />
+        {/* Render footer only on home and landing page */}
+        {showFooter && <Footer />}
       </div>
     </>
   );
