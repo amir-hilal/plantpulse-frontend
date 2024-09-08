d711087e8ece120240b0c5948e4b16bd2b152437
amirhilal044 <amiramirhilal@gmail.com>
2024-09-08 15:07:23 +0300

    updated GardenNav to trigger the modal

--- src/components/Gardens/GardenNav.jsx
+++ src/components/Gardens/GardenNav.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { GiPlantSeed } from 'react-icons/gi'; // Example icon for garden
-const GardenNav = ({ gardens, selectedGardenId, onSelectGarden }) => {
+const GardenNav = ({
+  gardens,
+  selectedGardenId,
+  onSelectGarden,
+  onAddGarden,
+}) => {
   return (
     <div className="text-xs sm:text-base mt-5 m-0 h-screen w-full">
       <ul className="list-none p-0 m-0 pl-2">
-        <li className="cursor-pointer mb-3 p-2 sm:pl-4 border-round-left-3xl flex align-items-center justify-content-between">
+        <li
+          className="cursor-pointer mb-3 p-2 sm:pl-4 border-round-left-3xl flex align-items-center justify-content-between"
+          onClick={onAddGarden}
+        >
           My Gardens
           <span>
             <FiPlus />
@@ -22,7 +30,7 @@ const GardenNav = ({ gardens, selectedGardenId, onSelectGarden }) => {
             onClick={() => onSelectGarden(garden.id)}
           >
             <GiPlantSeed className=" mr-2" />
-            <span className='sm:ml-1 md:ml-5'>{garden.name}</span>
+            <span className="sm:ml-1 md:ml-5">{garden.name}</span>
           </li>
         ))}
       </ul>
