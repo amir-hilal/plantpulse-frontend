5d2e301e61d3a81785f0aa06cdb78f08cc07f193
amirhilal044 <amiramirhilal@gmail.com>
2024-09-08 15:04:21 +0300

    updated gardenSlice to handle addGardens

--- src/features/garden/gardensSlice.js
+++ src/features/garden/gardensSlice.js
@@ -1,15 +1,34 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import api from '../../services/api'; // Axios instance for API requests
 
 // Async thunk for fetching gardens
-export const fetchGardens = createAsyncThunk('gardens/fetchGardens', async (_, { rejectWithValue }) => {
-  try {
-    const response = await api.get('/garden');
-    return response.data;
-  } catch (error) {
-    return rejectWithValue(error.response.data);
+export const fetchGardens = createAsyncThunk(
+  'gardens/fetchGardens',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await api.get('/garden');
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
   }
-});
+);
+
+export const addGarden = createAsyncThunk(
+  'gardens/addGarden',
+  async (formData, { rejectWithValue }) => {
+    try {
+      const response = await api.post('/garden', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
 const gardensSlice = createSlice({
   name: 'gardens',
@@ -34,6 +53,17 @@ const gardensSlice = createSlice({
         state.gardens = action.payload;
       })
       .addCase(fetchGardens.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(addGarden.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(addGarden.fulfilled, (state, action) => {
+        state.loading = false;
+        state.gardens.push(action.payload);
+      })
+      .addCase(addGarden.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
       });
