d13e6838d6d90581d10299f0e5d8072296ea4b9d
amirhilal044 <amiramirhilal@gmail.com>
2024-09-08 15:05:45 +0300

    Revert "updated GardenNav to trigger the modal"
    This reverts commit 715677a761c4a75b0c75a9de9b18679d3626e6bc.

--- src/components/Gardens/GardenNav.jsx
+++ src/components/Gardens/GardenNav.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
-import { GiPlantSeed } from 'react-icons/gi';
-
-const GardenNav = ({ gardens, selectedGardenId, onSelectGarden, onAddGarden }) => {
+import { GiPlantSeed } from 'react-icons/gi'; // Example icon for garden
+const GardenNav = ({ gardens, selectedGardenId, onSelectGarden }) => {
   return (
-    <div className="grid m-0">
-      <ul className="list-none p-0">
-        <li
-          onClick={onAddGarden} // Trigger modal on click
-          className="flex align-items-center justify-content-between"
-        >
-          <span>My Gardens</span>
-          <FiPlus />
+    <div className="text-xs sm:text-base mt-5 m-0 h-screen w-full">
+      <ul className="list-none p-0 m-0 pl-2">
+        <li className="cursor-pointer mb-3 p-2 sm:pl-4 border-round-left-3xl flex align-items-center justify-content-between">
+          My Gardens
+          <span>
+            <FiPlus />
+          </span>
         </li>
         {gardens.map((garden) => (
           <li
             key={garden.id}
-            className={`flex align-items-center ${
-              garden.id === selectedGardenId ? 'text-bold' : ''
+            className={`cursor-pointer mb-3 p-2 sm:pl-4 border-round-left-3xl flex align-items-center  bg-secondary text-tint-5 border-solid border-secondary border-right-none ${
+              garden.id === selectedGardenId
+                ? ' bg-tint-4 text-color-secondary'
+                : ''
             }`}
             onClick={() => onSelectGarden(garden.id)}
           >
-            <GiPlantSeed />
-            <span>{garden.name}</span>
+            <GiPlantSeed className=" mr-2" />
+            <span className='sm:ml-1 md:ml-5'>{garden.name}</span>
           </li>
         ))}
       </ul>
