715677a761c4a75b0c75a9de9b18679d3626e6bc
amirhilal044 <amiramirhilal@gmail.com>
2024-09-08 15:03:17 +0300

    updated GardenNav to trigger the modal

--- src/components/Gardens/GardenNav.jsx
+++ src/components/Gardens/GardenNav.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
-import { GiPlantSeed } from 'react-icons/gi'; // Example icon for garden
-const GardenNav = ({ gardens, selectedGardenId, onSelectGarden }) => {
+import { GiPlantSeed } from 'react-icons/gi';
+
+const GardenNav = ({ gardens, selectedGardenId, onSelectGarden, onAddGarden }) => {
   return (
-    <div className="text-xs sm:text-base mt-5 m-0 h-screen w-full">
-      <ul className="list-none p-0 m-0 pl-2">
-        <li className="cursor-pointer mb-3 p-2 sm:pl-4 border-round-left-3xl flex align-items-center justify-content-between">
-          My Gardens
-          <span>
-            <FiPlus />
-          </span>
+    <div className="grid m-0">
+      <ul className="list-none p-0">
+        <li
+          onClick={onAddGarden} // Trigger modal on click
+          className="flex align-items-center justify-content-between"
+        >
+          <span>My Gardens</span>
+          <FiPlus />
         </li>
         {gardens.map((garden) => (
           <li
             key={garden.id}
-            className={`cursor-pointer mb-3 p-2 sm:pl-4 border-round-left-3xl flex align-items-center  bg-secondary text-tint-5 border-solid border-secondary border-right-none ${
-              garden.id === selectedGardenId
-                ? ' bg-tint-4 text-color-secondary'
-                : ''
+            className={`flex align-items-center ${
+              garden.id === selectedGardenId ? 'text-bold' : ''
             }`}
             onClick={() => onSelectGarden(garden.id)}
           >
-            <GiPlantSeed className=" mr-2" />
-            <span className='sm:ml-1 md:ml-5'>{garden.name}</span>
+            <GiPlantSeed />
+            <span>{garden.name}</span>
           </li>
         ))}
       </ul>
