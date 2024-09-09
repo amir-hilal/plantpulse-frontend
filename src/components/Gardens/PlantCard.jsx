17c94202bc694f877c8651156d2aa99b7c2e60f2
amirhilal044 <amiramirhilal@gmail.com>
2024-09-09 11:48:52 +0300

    finished styling plant card

--- src/components/Gardens/PlantCard.jsx
+++ src/components/Gardens/PlantCard.jsx
@@ -16,7 +16,7 @@ const PlantCard = ({ plant }) => {
 
   return (
     <div
-      className="surface-card shadow-2 border-round-lg p-3 flex flex-column text-xs"
+      className="surface-card shadow-2 border-round-lg p-3 flex flex-column text-xs justify-content-between"
       style={{
         backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.9)), url(${plant.image_url})`,
         backgroundSize: 'cover',
@@ -39,29 +39,35 @@ const PlantCard = ({ plant }) => {
         </div>
       </div>
 
-      {/* Plant Name and Details */}
-      <div className="flex flex-column mt-5">
-        <div className="flex justify-content-between mb-2">
-          <span
-            className={`px-2 py-1 border-round-sm ${
-              plant.health_status === 'Healthy' ? 'bg-green-500' : 'bg-red-500'
-            }`}
-          >
-            {plant.health_status}
-          </span>
-          <span className="px-2 py-1 border-round-sm bg-gray-200 text-black">
-            {plant.category}
-          </span>
+      <div>
+        {/* Plant Name and Details */}
+        <div className="flex flex-column mt-5">
+          <div className="flex justify-content-between mb-2">
+            <span
+              className={`px-2 py-1 border-round-sm ${
+                plant.health_status === 'Healthy'
+                  ? 'bg-green-500'
+                  : 'bg-red-500'
+              }`}
+            >
+              {plant.health_status}
+            </span>
+            <span className="px-2 py-1 border-round-sm bg-gray-200 text-black">
+              {plant.category}
+            </span>
+          </div>
+
+          <div className=" mb-2 flex justify-content-between align-items-start">
+            <h2 className="text-left text-lg font-bold m-0">{plant.name}</h2>{' '}
+            <span className="text-right text-sm w-6rem">Last Time Watered</span>
+          </div>
+          <div className=" mb-2 flex justify-content-between align-items-start">
+            <p className="text-left m-0">{plant.age} years old</p>{' '}
+            <span className="text-right text-sm w-6rem">
+              {plant.last_watered}
+            </span>
+          </div>
         </div>
-
-        <div className="text-lg font-bold mb-2">{plant.name}</div>
-        <div className="text-md mb-2">{plant.age} years old</div>
-      </div>
-
-      {/* Last Watering Info */}
-      <div className="flex justify-content-between mt-auto text-sm">
-        <span>Last Time Watered</span>
-        <span>{plant.last_time_watered}</span>
       </div>
     </div>
   );
