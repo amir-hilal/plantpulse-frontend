c6d01e7a93f4663dd7e93c0e17879afcd6e1637e
amirhilal044 <amiramirhilal@gmail.com>
2024-09-09 18:23:38 +0300

    added a floating button on the right bottom to integrate a chatbot

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import ChatModal from './components/common/ChatModal';
+import FloatingChatButton from './components/common/FloatingChatButton';
 import VerifyEmailNotice from './components/common/VerifyEmailNotice';
 import Footer from './components/Layout/Footer';
 import NavBar from './components/Layout/NavBar';
@@ -12,6 +14,7 @@ const MainLayout = ({ children }) => {
   const isMenuOpen = useSelector((state) => state.ui.isMenuOpen);
   const dispatch = useDispatch();
   const location = useLocation(); // Get the current route
+  const [isChatOpen, setChatOpen] = useState(false);
 
   const handleOverlayClick = () => {
     if (isMenuOpen) {
@@ -37,6 +40,8 @@ const MainLayout = ({ children }) => {
         {/* Render footer only on home and landing page */}
         {showFooter && <Footer />}
       </div>
+      <FloatingChatButton onClick={() => setChatOpen(true)} />
+      <ChatModal isOpen={isChatOpen} onClose={() => setChatOpen(false)} />
     </>
   );
 };
--- src/components/common/FloatingChatButton.jsx
+++ src/components/common/FloatingChatButton.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaCommentDots } from 'react-icons/fa'; // Icon for the button
 
 const FloatingChatButton = ({ onClick }) => {
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-0 right-0 z-50 cursor-pointer mr-4 mb-4">
       <button
-        className="bg-orange-500 text-white p-4 rounded-full shadow-lg hover:bg-orange-600 transition duration-300"
+        className="bg-secondary border-round-3xl border-none hover:surface-800 text-white p-3 rounded-full shadow-lg  transition duration-300 flex align-items-center"
         onClick={onClick}
       >
-        <FaCommentDots size={24} />
+        <p className='pr-3 m-0'>Ask Flora</p>
+        <FaCommentDots size={18} />
       </button>
     </div>
   );
