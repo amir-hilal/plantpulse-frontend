6bd19d4eb332df6ffcbcd096eda1495616cd4154
amirhilal044 <amiramirhilal@gmail.com>
2024-09-15 01:09:41 +0300

    styling HomePage

--- src/components/common/MyGardensCarousel.jsx
+++ src/components/common/MyGardensCarousel.jsx
@@ -1,99 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { FaPlus } from 'react-icons/fa6';
+import { IoIosArrowForward } from 'react-icons/io';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { fetchGardens } from '../../features/garden/gardensSlice';
-
 const MyGardensCarousel = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { gardens, loading, error } = useSelector((state) => state.gardens);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const itemsToShow = 3; // Number of cards to show at once
 
   useEffect(() => {
     dispatch(fetchGardens());
   }, [dispatch]);
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % gardens.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + gardens.length) % gardens.length
-    );
-  };
-
   if (loading) return <p>Loading gardens...</p>;
   if (error) return <p>Error loading gardens: {error}</p>;
 
   const styles = {
-    carouselContainer: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'relative',
-      width: '100%',
-    },
-    carousel: {
-      overflow: 'hidden',
-      width: '80%',
-    },
-    carouselItems: {
-      display: 'flex',
-      transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)`,
-      transition: 'transform 0.3s ease-in-out',
-    },
-    card: {
-      minWidth: `${100 / itemsToShow}%`,
-      cursor: 'pointer',
-      padding: '10px',
-      borderRadius: '12px',
-      backgroundColor: '#f4f4f4', // surface-100 equivalent
-    },
     img: {
-      width: '100%',
       borderRadius: '12px',
-      height: 'auto',
-    },
-    button: {
-      background: 'none',
-      border: 'none',
-      fontSize: '2rem',
-      cursor: 'pointer',
-    },
+    }
   };
 
   return (
-    <div style={{ padding: '10px' }}>
-      <h2 style={{ marginBottom: '20px' }}>Go to My Gardens</h2>
-      <div style={styles.carouselContainer}>
-        <button style={styles.button} onClick={handlePrev}>
-          ◀
+    <div className="h-full cursor-pointer">
+      <div className="flex justify-content-between mt-3 mb-4">
+        <h2
+          className="flex align-items-center m-0"
+          onClick={() => navigate(`/my-gardens`)}
+        >
+          Go to My Gardens <IoIosArrowForward className="ml-2" />
+        </h2>
+        <button
+          onClick={() => navigate('/my-gardens')}
+          className=" text-sm bg-primary border-round-lg border-solid border-primary hover:bg-primary-reverse py-2  flex align-items-center justify-content-center cursor-pointer ml-1 md:ml-0"
+        >
+          <FaPlus className="mr-2" />
+          Add Garden
         </button>
-        <div style={styles.carousel}>
-          <div style={styles.carouselItems}>
-            {gardens
-              .slice(currentIndex, currentIndex + itemsToShow)
-              .map((garden) => (
-                <div
-                  key={garden.id}
-                  style={styles.card}
-                  onClick={() => navigate(`/my-gardens`)}
-                >
-                  <img
-                    src={garden.image_url}
-                    alt={garden.name}
-                    style={styles.img}
-                  />
-                  <h3>{garden.name}</h3>
-                </div>
-              ))}
-          </div>
+      </div>
+
+      <div
+        className="flex align-items-center justify-content-center w-full"
+        onClick={() => navigate(`/my-gardens`)}
+      >
+        <div
+          style={styles.carouselItems}
+          className="flex w-full overflow-scroll"
+        >
+          {gardens.map((garden) => (
+            <div key={garden.id} style={styles.card} className="mr-4">
+              <img
+                src={garden.image_url}
+                alt={garden.name}
+                style={styles.img}
+                width={170}
+                height={170}
+              />
+              <h3>{garden.name}</h3>
+            </div>
+          ))}
         </div>
-        <button style={styles.button} onClick={handleNext}>
-          ▶
-        </button>
       </div>
     </div>
   );
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MyGardensCarousel from '../components/common/MyGardensCarousel';
 import PostCard from '../components/posts/PostCard';
@@ -12,6 +12,9 @@ const HomePage = () => {
   const posts = useSelector((state) => state.posts.posts);
   const loading = useSelector((state) => state.posts.loading);
   const error = useSelector((state) => state.posts.error);
+  const user = useSelector((state) => state.auth.userProfile);
+
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     // Clear previous posts when the component is mounted
@@ -23,21 +26,40 @@ const HomePage = () => {
 
   return (
     <div className="home-page">
+      <section className="flex px-8 my-3 align-items-center justify-content-between">
+        <h3 className="text-primary">
+          Welcom back,{' '}
+          <span className="text-secondary">
+            {user.first_name} {user.last_name}
+          </span>
+        </h3>
+        <div className="px-2">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search..."
+            className="py-2 pl-6 w-full surface-100 border-solid surface-border border-round-xl appearance-none outline-none focus:border-primary"
+          />
+        </div>
+      </section>
       {/* Gardens Carousel */}
-      <section className="my-gardens-section p-3">
+      <section className=" px-6 py-2  mx-8 h-20rem bg-tint-5 border-round-xl mb-5">
         <MyGardensCarousel />
       </section>
 
       {/* Community Posts */}
-      <section className="community-section p-3">
-        <h2>Community Posts</h2>
+      <section className=" px-6 py-2  mx-8 h-20rem bg-tint-5 border-round-xl mb-5">
+        <div className="h-full">
+          <h2>Community Posts</h2>
 
-        {loading && <p>Loading posts...</p>}
-        {error && <p>Error loading posts: {error}</p>}
+          {loading && <p>Loading posts...</p>}
+          {error && <p>Error loading posts: {error}</p>}
 
-        {!loading && posts.length > 0
-          ? posts.map((post) => <PostCard key={post.id} post={post} />)
-          : !loading && <p>No posts available</p>}
+          {!loading && posts.length > 0
+            ? posts.map((post) => <PostCard key={post.id} post={post} />)
+            : !loading && <p>No posts available</p>}
+        </div>
       </section>
     </div>
   );
