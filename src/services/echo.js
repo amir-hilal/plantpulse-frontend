7e7a2a8363ad4af0c498d35ce204af6c313dbf5f
amirhilal044 <amiramirhilal@gmail.com>
2024-09-14 09:13:57 +0300

    updated echo.js

--- src/services/echo.js
+++ src/services/echo.js
@@ -1,13 +1,20 @@
-import Echo from "laravel-echo";
-import Pusher from "pusher-js";
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
 const echo = new Echo({
-  broadcaster: 'pusher',
-  key: process.env.REACT_APP_PUSHER_APP_KEY,
-  cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER,
-  forceTLS: true,
+    broadcaster: 'pusher',
+    key: process.env.REACT_APP_PUSHER_APP_KEY,
+    cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    encrypted: true,
+    authEndpoint: `${process.env.REACT_APP_API_URL}/broadcasting/auth`, // Ensure your API URL is correct
+    auth: {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}` // or however you handle auth
+        }
+    }
 });
 
 export default echo;
